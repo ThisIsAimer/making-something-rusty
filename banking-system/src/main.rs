291d1c77fa8b7c17ed092729203f65f8d170b8b4
raunak <raunakbiswas05@gmail.com>
2025-05-02 22:29:23 +0530

    finished the bank project

--- banking-system/src/main.rs
+++ banking-system/src/main.rs
@@ -9,6 +9,19 @@ impl Account{
     fn new(id:i32, holder:String) -> Self{
         Account { id, balance: 0, holder }
     }
+
+    fn deposit(&mut self, amount: i32){
+        self.balance+=amount;
+    }
+
+    fn withdraw(&mut self, amount: i32){
+        self.balance-=amount;
+    }
+    fn summary(&self) -> String{
+        //println!("Account ID:{}, Holder name:{}, balance:{}", self.id,self.holder,self.balance);
+        format!("Account ID:{}, Holder name:{}, balance:{}", self.id,self.holder,self.balance)
+        //format! forms a string!
+    }
 }
 
 #[derive(Debug)]
@@ -20,11 +33,21 @@ impl Bank{
     fn new() -> Self{
         Bank { accounts: vec![] }
     }
+
+    fn add_account(&mut self,account:Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32{
+        self.accounts.iter().map(|account| account.balance).sum()
+        //self.accounts.iter().map(|item| item.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String>{
+        self.accounts.iter().map(|account| account.summary()).collect() //or collect::<Vec<String>>()
+    }
 }
 
-fn print_account(account:&Account){
-    println!("{:#?}",account);
-}
 
 
 
@@ -32,16 +55,19 @@ fn main() {
     let mut bank = Bank::new();
     let mut account = Account::new(1, "ABCD".to_string());
                                                         // we can also use String::from("ABCD")
-    account.balance=400;
-    bank.accounts.push(account.clone());
+    account.deposit(1000);
+    account.id=7000;
+    account.holder="cat".to_string();
 
-    let account_ref = &mut account; //changing the data through borrowing
-    account_ref.id=7000;
-    account_ref.holder="cat".to_string();
+    let mut account1 = Account::new(300, "ABCD".to_string());
+    account1.deposit(1000);
 
+    bank.add_account(account);
+    bank.accounts[0].withdraw(100);
+    bank.add_account(account1);
 
-    print_account(&account);
     println!("{:#?}", bank);
-    println!("{:#?}", account);
-    
+    println!("{}",bank.accounts[0].summary());
+    println!("\ntotal balance in bank:{}",bank.total_balance());
+    println!("\ntotal balance in bank:{:#?}",bank.summary());
 }
