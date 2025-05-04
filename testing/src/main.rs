36c3059c6fb372bfb86cfde5ce243c5f3b05b431
raunak <raunakbiswas05@gmail.com>
2025-05-04 11:14:17 +0530

    options with Option<&mut --->

---
+++ testing/src/main.rs
@@ -0,0 +1,36 @@
+
+#[derive(Debug)]
+struct Account {
+    balance: i32
+}
+
+fn first_muts(accounts: &mut Vec<Account>) -> Option<&mut Account>{
+    if accounts.len()>1{
+        Some(&mut accounts[0])
+    }
+    else{
+        None
+    }
+}
+
+fn main() {
+    let mut accounts: Vec<Account> = vec![
+        Account { balance: 0 },
+        Account { balance: 10 }
+    ];
+
+    match first_muts(&mut accounts) {
+
+        Some(value) => {
+            value.balance = 30;
+            println!("{:#?}",accounts);
+        }
+
+        None => {
+            println!("nothing")
+        }
+        
+    }
+    
+    // Add code here:
+}
