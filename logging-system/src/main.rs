d8708941618235e224a2c1fbf598befb5e16f897
raunak <raunakbiswas05@gmail.com>
2025-05-04 21:33:12 +0530

    error handling with Result enum!

--- logging-system/src/main.rs
+++ logging-system/src/main.rs
@@ -1,7 +1,26 @@
-use std::fs;
+use std::{fs, io::Error};// import io::Error for Error::other
 
 fn main() {
     let text = fs::read_to_string("logs.txt");
     println!("{:#?}",text);
+
+    match devide(3.0, 0.0){
+        Ok(value) => {
+            print!("{}",value)
+        }
+        Err(value) =>{
+            print!("{value}")
+        }
+    }
     
+}
+
+fn devide(a:f64,b:f64)-> Result<f64,Error>{
+    if b == 0.0 {
+        Err(Error::other("cant devide by zero"))
+    }
+    else{
+        Ok(a/b)
+    }
+
 }
