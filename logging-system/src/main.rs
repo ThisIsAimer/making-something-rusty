29595b336d94b9c050a302624951951af58669ba
raunak <raunakbiswas05@gmail.com>
2025-05-05 12:43:19 +0530

    Result enum on reading files

--- logging-system/src/main.rs
+++ logging-system/src/main.rs
@@ -1,47 +1,52 @@
 use std::{fs, io::Error};// import io::Error for Error::other
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
-    println!("{:#?}",text);
+    match fs::read_to_string("logs.txt") {
 
-    match devide(3.0, 0.0){
-        Ok(value) => {
-            println!("the value is: {}",value)
-        }
-        Err(value) =>{
-            println!("{value}")
-        }
+        Ok(text)=> {println!("text that was read:-\n{}",text)}
+        Err(error) =>{println!("error found: {}", error)}
     }
+
     
-    match validate_email("abca@abcd.com".to_string()) {
 
-        Ok(()) => { //or ..
-            println!("mail is valid")
-        }
+    // match devide(3.0, 0.0){
+    //     Ok(value) => {
+    //         println!("the value is: {}",value)
+    //     }
+    //     Err(value) =>{
+    //         println!("{value}")
+    //     }
+    // }
+    
+    // match validate_email("abca@abcd.com".to_string()) {
 
-        Err(value) => {
-            println!("{}",value)
-        }
+    //     Ok(()) => { //or ..
+    //         println!("mail is valid")
+    //     }
+
+    //     Err(value) => {
+    //         println!("{}",value)
+    //     }
         
-    }
+    // }
 
 }
 
-fn validate_email(email:String) -> Result<(), Error>{
-    if email.contains("@"){
-        Ok(()) //() is an empty touple
-    }
-    else{
-        Err(Error::other("email must contain a @"))
-    }
-}
+// fn validate_email(email:String) -> Result<(), Error>{
+//     if email.contains("@"){
+//         Ok(()) //() is an empty touple
+//     }
+//     else{
+//         Err(Error::other("email must contain a @"))
+//     }
+// }
 
-fn devide(a:f64,b:f64)-> Result<f64,Error>{
-    if b == 0.0 {
-        Err(Error::other("cant devide by zero"))
-    }
-    else{
-        Ok(a/b)
-    }
+// fn devide(a:f64,b:f64)-> Result<f64,Error>{
+//     if b == 0.0 {
+//         Err(Error::other("cant devide by zero"))
+//     }
+//     else{
+//         Ok(a/b)
+//     }
 
-}
+// }
