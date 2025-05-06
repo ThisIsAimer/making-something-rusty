57a62648da35c6eac29bf376c2377c34b95cfae8
raunak <raunakbiswas05@gmail.com>
2025-05-06 14:53:23 +0530

    file writing

--- logging-system/src/main.rs
+++ logging-system/src/main.rs
@@ -26,43 +26,12 @@ fn main() {
     }
     println!("{:#?}", error_lines);
 
-    // match devide(3.0, 0.0){
-    //     Ok(value) => {
-    //         println!("the value is: {}",value)
-    //     }
-    //     Err(value) =>{
-    //         println!("{value}")
-    //     }
-    // }
-
-    // match validate_email("abca@abcd.com".to_string()) {
-
-    //     Ok(()) => { //or ..
-    //         println!("mail is valid")
-    //     }
-
-    //     Err(value) => {
-    //         println!("{}",value)
-    //     }
-
-    // }
+    match fs::write("errors.txt", error_lines.join("\n")) {
+        Ok(()) =>{
+            println!("writing was successfull")
+        }
+        Err(error) => {
+            println!("error: {}", error);
+        }
+    }
 }
-
-// fn validate_email(email:String) -> Result<(), Error>{
-//     if email.contains("@"){
-//         Ok(()) //() is an empty touple
-//     }
-//     else{
-//         Err(Error::other("email must contain a @"))
-//     }
-// }
-
-// fn devide(a:f64,b:f64)-> Result<f64,Error>{
-//     if b == 0.0 {
-//         Err(Error::other("cant devide by zero"))
-//     }
-//     else{
-//         Ok(a/b)
-//     }
-
-// }
