2bf00e55b6f4dea20a897735998886364424189e
raunak <raunakbiswas05@gmail.com>
2025-05-06 20:07:53 +0530

    code with ? notation

--- logging-system/src/main.rs
+++ logging-system/src/main.rs
@@ -1,37 +1,30 @@
-use std::fs; // import io::Error for Error::other
+use std::{io::Error, fs}; // import io::Error for Error::other
 
-fn extract_errors(text: &str) -> Vec<String> {
+fn extract_errors(text: &str) -> Vec<&str> {
     let split_text = text.split("\n");
 
     let mut results = vec![];
 
     for lines in split_text {
         if lines.starts_with("ERROR") {
-            results.push(lines.to_string());
+            results.push(lines);
         }
     }
 
     results
 }
 
-fn main() {
-    let mut error_lines = vec![];
-    match fs::read_to_string("logs.txt") {
-        Ok(text) => {
-            error_lines = extract_errors(&text);
-        }
-        Err(error) => {
-            println!("error found: {}", error);
-        }
-    }
-    println!("{:#?}", error_lines);
+fn main() -> Result<(),Error> {
+     let text = fs::read_to_string("logs.txt")?;//? is try notation
+     let errors_text = extract_errors(&text);
+     fs::write("errors.txt", errors_text.join("\n"))?;
 
-    match fs::write("errors.txt", error_lines.join("\n")) {
-        Ok(()) =>{
-            println!("writing was successfull")
-        }
-        Err(error) => {
-            println!("error: {}", error);
-        }
-    }
+
+     Ok(()) // gives error when ? fails. but doesnt give OK value so, we state Ok
+
+     /*or 
+     let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+     let errors_text = extract_errors(&text);
+     fs::write("errors.txt", errors_text.join("\n")).expect("failed to write errors");
+      */
 }
