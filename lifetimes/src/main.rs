a360e89aef94b041ba374650908c8f4f4f86c2c3
raunak <raunakbiswas05@gmail.com>
2025-05-09 20:04:25 +0530

    lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,29 @@
+/* when a function has two or more references as parameters and returns a 
+reference, rust will assume that the reference points to one of the paramers
+as <'a> and &'a are supposed to be put to notify rust that its that parameter*/
+fn next_lang<'a>(elements : &'a[String], value: &str)-> &'a str{
+    let mut found = false;
+
+    for element in elements{
+        if element == value{
+            found = true;
+        }
+        if found{
+            return element;
+        }
+    }
+
+    elements.last().unwrap()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let lang = vec![
+        "python".to_string(),
+        "Rust".to_string(),
+        "Go".to_string()
+    ];
+
+    let nextlang = next_lang(&lang, "Rust");
+    print!("{:#?}",nextlang)
 }
