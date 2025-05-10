e88b8fab9db66dd65a6a5eb9014b39c617e656ea
raunak <raunakbiswas05@gmail.com>
2025-05-10 19:45:03 +0530

    now we can give f32 and 64 as arguments

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,6 +1,6 @@
 use num_traits::{Float,ToPrimitive};
 
-fn hypotenuse<T:Float>(a:T,b:T) -> f64 {
+fn hypotenuse<T:Float,U:Float>(a:T,b:U) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
     (a_f64.powi(2)+b_f64.powi(2)).sqrt()
@@ -8,7 +8,7 @@ fn hypotenuse<T:Float>(a:T,b:T) -> f64 {
 
 fn main() {
 
-    let a: f32 = 3.0;
+    let a: f64 = 3.0;
     let b: f32 = 4.0;
 
 
