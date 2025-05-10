64a0794e368ba52b8f31f97caadab9230510b5fd
raunak <raunakbiswas05@gmail.com>
2025-05-10 19:11:16 +0530

    using generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,16 +1,16 @@
-use num_traits::ToPrimitive;
+use num_traits::{Float,ToPrimitive};
 
-fn hypotenuse(a:f64,b:f64) -> f64 {
-    (a.powi(2)+b.powi(2)).sqrt()
+fn hypotenuse<T:Float>(a:T,b:T) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+    (a_f64.powi(2)+b_f64.powi(2)).sqrt()
 }
 
 fn main() {
 
     let a: f32 = 3.0;
-    let b: f64 = 4.0;
-
-    let a_f64 = a.to_f64().unwrap();
+    let b: f32 = 4.0;
 
 
-    println!("{}", hypotenuse(a_f64, b));
+    println!("{}", hypotenuse(a, b));
 }
